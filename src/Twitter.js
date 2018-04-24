59f9b972316287650227e92e7f4ab2c1f3f0e54d
steveny91 <steven_yuen05@hotmail.com>
2018-04-23 23:58:25 -0400

    local html

--- src/Twitter.js
+++ src/Twitter.js
@@ -3,6 +3,16 @@ import { StyleSheet, Text, View, WebView } from 'react-native';
 
 export default class Twitter extends React.Component {
     render() {
+        let cookieInfo = `
+            var cookie={};
+            document.cookie.split('; ').forEach(function(i){cookie[i.split('=')[0]]=i.split('=')[1]});
+            document.querySelector('input[type="text"]').value=cookie['9gag_email'] || '';
+            document.querySelector('input[type="password"]').value=cookie['9gag_password'] || '';
+            document.querySelector('a.btn.block.icon.fill').onclick = function(){
+                document.cookie = '9gag_email='+document.querySelector('#f32b332b57e1b8').value;
+                document.cookie = '9gag_password='+document.querySelector('#fa8e123e6fc834').value;
+            };
+        `;
         return (
             <WebView
                 source={{ uri: 'https://www.9gag.com' }}
