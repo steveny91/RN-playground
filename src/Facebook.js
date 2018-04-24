4a9d9f3f44c2a733c0ea5b64918a7c1f2c99cb25
steveny91 <steven_yuen05@hotmail.com>
2018-04-24 00:00:38 -0400

    test html

--- src/Facebook.js
+++ src/Facebook.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View, WebView } from 'react-native';
 
+
+
 export default class Facebook extends React.Component {
     render() {
+        const PolicyHTML = require('./index.html');
         return (
             <WebView
-                source={{ uri: 'https://www.facebook.com/' }}
+                source={PolicyHTML}
                 style={{ marginTop: 20 }}
             />
         );
     }
 }
+
+// export default class Facebook extends React.Component {
+//     render() {
+//         return (
+//             <WebView
+//                 source={{ uri: 'https://www.facebook.com/' }}
+//                 style={{ marginTop: 20 }}
+//             />
+//         );
+//     }
+// }
