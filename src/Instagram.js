4ae915f60c8ef49bb6aa129689e4e36342a1a375
steveny91 <steven_yuen05@hotmail.com>
2018-04-29 23:53:34 -0400

    local html

--- src/Instagram.js
+++ src/Instagram.js
@@ -3,9 +3,19 @@ import { StyleSheet, Text, View, WebView } from 'react-native';
 
 export default class Instagram extends React.Component {
     render() {
+        let cookieInfo = `
+            var cookie={};
+            document.cookie.split('; ').forEach(function(i){cookie[i.split('=')[0]]=i.split('=')[1]});
+            document.querySelector('#f32b332b57e1b8').value=cookie['instagram_email'] || '';
+            document.querySelector('#fa8e123e6fc834').value=cookie['instagram_password'] || '';
+            document.querySelector('._t38eb').onclick = function(){
+                document.cookie = 'instagram_email='+document.querySelector('#f32b332b57e1b8').value;
+                document.cookie = 'instagram_password='+document.querySelector('#fa8e123e6fc834').value;
+            };
+        `;
         return (
             <WebView
-                source={{ uri: 'https://www.instagram.com/realgrumpycat/' }}
+                source={{ uri: 'https://www.instagram.com/' }}
                 style={{ marginTop: 20 }}
             />
         );
--- src/Twitter.js
+++ src/Twitter.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, WebView } from 'react-native';
 
+
+
 export default class Twitter extends React.Component {
     render() {
+        const PolicyHTML = require('./index.html');
         return (
             <WebView
-                source={{ uri: 'https://twitter.com/NBA' }}
+                source={PolicyHTML}
                 style={{ marginTop: 20 }}
             />
         );
